c07319ca1093a9c65b7d2bd7886e6105cf0f2e91
Sirin Alkadi <92253615+SirinK2@users.noreply.github.com>
2023-06-17 15:14:21 +0300

    Update form_dailog.dart

--- lib/feature/product/view/widget/form_dailog.dart
+++ lib/feature/product/view/widget/form_dailog.dart
@@ -6,14 +6,6 @@ class FormDialog extends StatelessWidget {
   FormDialog({Key? key}) : super(key: key);
   final controller = Get.find<ProductController>();
 
-/*
-"title": "New Product",
-  "price": 10,
-  "description": "A description",
-  "categoryId": 1,
-  "images": ["https://placeimg.com/640/480/any"]
-}
- */
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
